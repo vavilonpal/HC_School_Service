c104d91f206f8f97e5319fefc2f58452e8b49dcb
Alexeev <sergey50878@mail.ru>
2024-12-14 15:50:21 +0200

    project initialize commit

---
+++ src/main/java/org/combs/hc_school_service/HcSchoolServiceApplication.java
@@ -0,0 +1,13 @@
+package org.combs.hc_school_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcSchoolServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcSchoolServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/combs/hc_school_service/HcSchoolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.combs.hc_school_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcSchoolServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
